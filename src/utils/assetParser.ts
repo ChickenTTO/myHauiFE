75d9a24a29234638825f306e517c8a248066970a
ChickenTTO <datchutich69@gmail.com>
2026-05-21 15:19:44 +0700

    fix: import xlsx using esm to fix exports is not defined error

--- src/utils/assetParser.ts
+++ src/utils/assetParser.ts
@@ -1,4 +1,4 @@
-import * as XLSX from 'xlsx';
+import * as XLSX from 'xlsx/xlsx.mjs';
 
 export const parseAssetExcel = (file) => {
   return new Promise((resolve, reject) => {
--- src/utils/excelParser.ts
+++ src/utils/excelParser.ts
@@ -1,4 +1,4 @@
-import * as XLSX from 'xlsx';
+import * as XLSX from 'xlsx/xlsx.mjs';
 
 export const parseTimetableExcel = async (file) => {
   return new Promise((resolve, reject) => {
